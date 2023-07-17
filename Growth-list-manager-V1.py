2855c5aeb589234dba9a9690e167ecb194d88ea2
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-07-17 10:53:10 +0200

    Update Growth-list-manager-V1.py
    Added code for google API authetication

--- Growth-list-manager-V1.py
+++ Growth-list-manager-V1.py
@@ -1,3 +1,16 @@
+### Import libraries
 import streamlit as st
 import gspread
 from google.oauth2 import service_account
+import json
+
+
+### Parse credentials stored in Streamlit Share secret
+# Load JSON credentials as a dictionary
+credentials_dict = json.loads(google_credentials)
+
+# Create ServiceAccountCredentials object
+creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict)
+
+# Authenticate
+client = gspread.authorize(creds)
