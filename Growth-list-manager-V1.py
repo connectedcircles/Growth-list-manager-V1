21ff3ad181d2ace17fc0902311b7849a95f71971
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-07-17 11:03:29 +0200

    Update Growth-list-manager-V1.py
    Added a first version of the body

--- Growth-list-manager-V1.py
+++ Growth-list-manager-V1.py
@@ -22,3 +22,35 @@ sheet = client.open('Growth list manager login credentials').sheet1
 
 
 
+### Create a function to load the client credentials and URLs of the folder with their lists
+def login(name, password):
+    data = sheet.get_all_records()
+    for row in data:
+        if row['Name'] == name and row['Password'] == password:
+            return row['folder URL']
+    return None
+
+
+### Define the app
+def main():
+    st.title('Login')
+    name = st.text_input('Name')
+    password = st.text_input('Password', type='password')
+    if st.button('Login'):
+        folder_url = login(name, password)
+        if folder_url:
+            st.success('Login Successful!')
+            # Display clickable URLs to files in the folder
+            # You can use the Google Drive API or any other method to retrieve the files.
+            # Display them using Streamlit components like st.markdown or st.write.
+        else:
+            st.error('Invalid credentials')
+
+
+### Run the app
+if __name__ == '__main__':
+    main()
+
+
+
+
