ea9efbfc373861a6a0f1aaca0c0f712c2bc2bdd8
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-07-17 15:53:24 +0200

    Update Growth-list-manager-V1.py

--- Growth-list-manager-V1.py
+++ Growth-list-manager-V1.py
@@ -14,7 +14,7 @@ google_credentials_text = os.getenv('google_credentials_text')
 
 ### Define the app
 def main():
-    st.title(print(google_credentials_text))
+    st.text(print(google_credentials_text))
 
 
 
