223144c33d848c9fb0b5bfa5e84168795427f784
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-07-17 15:50:56 +0200

    Update Growth-list-manager-V1.py
    test

--- Growth-list-manager-V1.py
+++ Growth-list-manager-V1.py
@@ -10,47 +10,12 @@ import os
 # Load JSON credentials from a Streamlit Share secret
 google_credentials_text = os.getenv('google_credentials_text')
 
-# Convert to a dictionary
-google_credentials = json.loads(google_credentials_text)
-
-
-
-# Create ServiceAccountCredentials object
-creds = service_account.Credentials.from_service_account_info(google_credentials)
-
-# Authenticate
-client = gspread.authorize(creds)
-
-
-### Load the table of client login credentials
-sheet = client.open('Growth list manager login credentials').sheet1
-
-
-
-
-### Create a function to load the client credentials and URLs of the folder with their lists
-def login(name, password):
-    data = sheet.get_all_records()
-    for row in data:
-        if row['Name'] == name and row['Password'] == password:
-            return row['folder URL']
-    return None
 
 
 ### Define the app
 def main():
-    st.title('Login')
-    name = st.text_input('Name')
-    password = st.text_input('Password', type='password')
-    if st.button('Login'):
-        folder_url = login(name, password)
-        if folder_url:
-            st.success('Login Successful!')
-            # Display clickable URLs to files in the folder
-            # You can use the Google Drive API or any other method to retrieve the files.
-            # Display them using Streamlit components like st.markdown or st.write.
-        else:
-            st.error('Invalid credentials')
+    st.title('google_credentials_text')
+
 
 
 ### Run the app
