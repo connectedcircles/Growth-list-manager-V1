4c3a7083d2d606fbec5802813778251b0e5ac9ee
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-12-01 12:09:25 +0100

    revert to non-suggestion version 02Internal_invite_logger.py

--- pages/02Internal_invite_logger.py
+++ pages/02Internal_invite_logger.py
@@ -6,19 +6,16 @@ from google.oauth2 import service_account
 import pygsheets
 import json
 from slack_sdk import WebClient
-
 ### Google Drive API authentication (Streamlit share version) ###############################################
 # Authenticate Google Drive API
 # Authenticate Google Sheets API
 raw_creds = st.secrets["raw_creds"]
 json_creds = json.loads(raw_creds)
-
 creds = service_account.Credentials.from_service_account_info(
     json_creds,
     scopes=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 )
 ############################################################################################################
-
 ### Google Drive API authentication (Offline, local version) #################################################
 # Authenticate Google Drive API
 # Authenticate Google Sheets API
@@ -27,35 +24,23 @@ creds = service_account.Credentials.from_service_account_info(
 #    scopes=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 #)
 ############################################################################################################
-
-
-
 ### Slack authorization (Offline, local version) ####################################################################
-
 ############################################################################################
-
 ### Slack authorization (Streamlit share version) ####################################################################
 # set Slack access token
 slack_token = st.secrets["slack_token"]
 # set slack channel
 target_channel_id = st.secrets["target_channel_id"]
 ############################################################################################
-
-
-
 # Initialize the Slack WebClient
 slack_client = WebClient(token=slack_token)
-
 # Use Google credentials to authorize pygsheets
 gc = pygsheets.authorize(custom_credentials=creds)
-
-
-
 # Define function to append data to Google Sheet and send a Slack message
 def append_to_sheet(df, ClientName, Category, DateInvited_str, growth_list_url):
     try:
         # Open the Google Sheet by title
-        spreadsheet = gc.open_by_key('1UsI_uXcDeVQ61saUwok9wBygLbaEdR15zMCu52D5WU8')
+        spreadsheet = gc.open_by_key('1QWhVtOPoOoYVdxoUtM5RKAhwJKVW-6-q6T5ACZJVkdU')
         
         # Select the worksheet by title
         worksheet = spreadsheet.sheet1
@@ -67,15 +52,10 @@ def append_to_sheet(df, ClientName, Category, DateInvited_str, growth_list_url):
         worksheet.append_table(values=df.values.tolist(), start=f'A{start_row}', end=None, dimension='ROWS', overwrite=False)
         
         st.success("Data appended successfully!")
-
         # Send the message to Slack
         send_slack_message(message)
-
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
-
-
-
 def send_slack_message(message):
     try:
         # Send the message to Slack
@@ -83,65 +63,48 @@ def send_slack_message(message):
             channel= target_channel_id,
             text=message
         )
-
         if response["ok"]:
             st.success("Slack message sent successfully!")
         else:
             st.error(f"Failed to send Slack message. Error: {response['error']}")
     except Exception as e:
         st.error(f"An error occurred while sending the Slack message: {str(e)}")
-        
-        
-
-# Define a function to retrieve the Invited profiles from the Google Sheet
-def get_invited_profiles():
-    gc = pygsheets.authorize(custom_credentials=creds)
-    spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1QWhVtOPoOoYVdxoUtM5RKAhwJKVW-6-q6T5ACZJVkdU/edit#gid=1820302186")
-    worksheet = spreadsheet.worksheet_by_title("Invited")
-    records = worksheet.get_all_records()
-    df = pd.DataFrame(records)
-    return df
-
-
 # Streamlit app function
 def app():
+    # Set title and subtitle, additional text
     st.title("Growth Invite Logger V1")
     st.subheader("Property of Connected Circles")
-    st.write("Log invites with this app. Please mind your spelling. Data location: https://docs.google.com/spreadsheets/d/1QWhVtOPoOoYVdxoUtM5RKAhwJKVW-6-q6T5ACZJVkdU/edit#gid=1820302186. For emergency use, fill manually.")
+    st.write("""Log invites with this app. Please mind your spelling. Data location: https://docs.google.com/spreadsheets/d/1QWhVtOPoOoYVdxoUtM5RKAhwJKVW-6-q6T5ACZJVkdU/edit#gid=1820302186. For emergency use, fill mnaually""")
+    st.write("""Log invites with this app. Please mind your spelling. Data location: https://docs.google.com/spreadsheets/d/1QWhVtOPoOoYVdxoUtM5RKAhwJKVW-6-q6T5ACZJVkdU/edit#gid=1820302186. For emergency use, fill manually.""")
 
-    # Get invited profiles
-    invited_profiles = get_invited_profiles()
-
-    # Client name selection
-    client_names = invited_profiles['Client Name'].unique()
-    selected_client_name = st.selectbox("Select client name", client_names)
-
-    # Filter dataframe based on selected client name
-    filtered_df = invited_profiles[invited_profiles['Client Name'] == selected_client_name]
-
-    # Category selection based on filtered dataframe
-    categories = filtered_df['Category'].unique()
-    selected_category = st.selectbox("Select category", categories)
-
+    # Set client name, invite date, category, and growth list URL
+    ClientName = st.text_input("Enter client name")
     DateInvited = st.date_input("Select a date", datetime.date.today())
-    growth_list_url = st.text_input("Growth list URL")
+    Category = st.text_input("Enter category")
+    growth_list_url = st.text_input("Growth list URL")  # Added input for growth list URL
     
+    # Convert date to string format
     DateInvited_str = DateInvited.strftime('%Y-%m-%d')
-
+    # File uploader
     uploaded_file = st.file_uploader("Choose a CSV file to upload", type="csv")
-
+    
+    # Process data
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-        df.insert(0, "Client Name", selected_client_name)
+    
+        # Create a column for the name of the client
+        df.insert(0, "Client Name", ClientName)
+        # Rewrite the date and category columns
         df["Date collected"] = DateInvited_str
-        df["Category"] = selected_category
+        df["Category"] = Category
+        # Replace missing values with "NA"
         df.fillna("NA", inplace=True)
+        # Display both filtered and unfiltered data in two windows with links to download each below
         st.write(df)
-
+        # Button to append data to Google Sheet and send Slack message
         if st.button("Append data to Google Sheet and Send Slack Message"):
-            appended = append_to_sheet(df.dropna(how="all", axis=1), selected_client_name, selected_category, DateInvited_str, growth_list_url)
-            message = f"{selected_client_name}, {len(df)} profiles, \"{selected_category}\", {DateInvited_str}, {growth_list_url}"
+            appended = append_to_sheet(df.dropna(how="all", axis=1), ClientName, Category, DateInvited_str, growth_list_url)
+            message = f"{ClientName}, {len(df)} profiles, \"{Category}\", {DateInvited_str}, {growth_list_url}"
             send_slack_message(message)
-
 if __name__ == "__main__":
     app()
