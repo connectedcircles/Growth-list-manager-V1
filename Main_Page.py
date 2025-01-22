c117a97dcb5d5a8cb7f21d1611af7cbc63838606
connectedcircles <155981724+connectedcircles@users.noreply.github.com>
2025-01-22 11:01:37 +0100

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -3,13 +3,12 @@ import streamlit as st
 def app():
     st.title("Connected Circles Growth List Manager")
     st.subheader("Property of Connected Circles")
- st.write(
-    """
-    Welcome to the Growth List Manager! This tool helps streamline processes related to growth.  
-    For a detailed explanation of the pages in the app, please refer to the presentation:  
-    [Growth List Manager Presentation](https://docs.google.com/presentation/d/1usVEuWN_AeUbkhdDz5Ru4rR-a-3_fLJTLQzmvBi6KCE/edit#slide=id.g91cec8b3a2_0_0)
-    """
-)
+    st.write(
+        """
+        Welcome to the Growth List Manager! This tool helps streamline processes related to growth.  
+        For a detailed explanation of the pages in the app, please refer to the presentation:  
+        [Growth List Manager Presentation](https://docs.google.com/presentation/d/1usVEuWN_AeUbkhdDz5Ru4rR-a-3_fLJTLQzmvBi6KCE/edit#slide=id.g91cec8b3a2_0_0)
+        """)
 
 if __name__ == "__main__":
     app()
